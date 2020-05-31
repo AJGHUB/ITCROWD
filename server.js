da6a0bb9fe199371015037d1c090f84a56472980
Alice Jowsey <12345alcatraz@gmail.com>
2020-05-31 22:28:17 +1200

    db URI routes created work on postman

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express();
+const app = express(); //initialising express
+
+//Connect Database
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
 
 const PORT = process.env.PORT || 5000;
 
