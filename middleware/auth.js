8770af433f22243b5820d2d5bab10f83bc473f26
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-02 21:24:03 +1200

    custom auth middleware & JWT Verify * null error

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+    //get token from header
+    const token = req.header('x-auth-token');
+
+    //Check if no token
+    if(!token) {
+        return res.status(401).json({ msg: 'No token, authorization denied'});
+    }
+    
+    // Verify token > decoded by jwt to get users profile OR error
+    try {
+        const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token is not valid' })
+    }
+};
+
 
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
+const auth = require('../../middleware/auth');
+const User = require('../../models/User')
 
 // @route   GET api/auth
 // @desc    Test route 
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
-module.exports = router
+
+
+module.exports = router;
+
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNWVkNjAwN2IyMWYwMDQ1ZTg0MDJjM2JhIn0sImlhdCI6MTU5MTA4MzEzMSwiZXhwIjoxNTkxNDQzMTMxfQ.UJAG5tc9TAZy_1sShqRc2h9Bcvug8TFf0dMmb_FAknI
