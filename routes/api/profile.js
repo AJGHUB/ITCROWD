94c9cc4579a6e09635ddd249aa151c243664ab14
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-08 21:36:38 +1200

    api/profile user profile bug

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const {
+    check,
+    validationResult
+} = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
 
-// @route   GET api/profile/me
-// @desc    GET current users profile  
-// @access  Private
+
 router.get('/me', auth, async (req, res) => {
     try {
         const profile = await Profile.findOne({
@@ -31,5 +33,23 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route   POST api/profile
+// @desc    Create or update user profile 
+// @access  Private
+
+router.post('/', [auth, [
+        check('status', 'Status is required')
+        .not()
+        .isEmpty()
+    ]],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty)
+            return res.status(400).json({
+                errors: errors.array()
+            });
+    }
+);
 
 module.exports = router;
