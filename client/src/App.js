404cc0dc6318968ce2a64aaf84770c85f6846018
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-21 14:35:34 +1200

    react router links set up to pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 import './App.css';
 
 const App = () => 
-    <Fragment>
-     <Navbar/>
-     <Landing/>  
-    </Fragment>
+    <Router>
+        <Fragment>
+        <Navbar/>
+        <Route exact path="/" component={Landing} />
+            <Switch>
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+            </Switch>
+        </Fragment>
+    </Router>
 
 export default App;
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Register = () => {
+    return (
+        <div>
+            Register
+        </div>
+    )
+}
+export default Register;
