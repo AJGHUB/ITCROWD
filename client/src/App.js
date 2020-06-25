d4c7c5f3d822e748100a187923b93cb503925efa
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-25 14:49:45 +1200

    Alert component & action call with timeout

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
 //Redux
 import { Provider } from 'react-redux';
 import store from './store';
@@ -16,10 +18,13 @@ const App = () => (
       <Fragment>
         <Navbar />
         <Route exact path='/' component={Landing} />
+        <section className='container'>
+        <Alert />
         <Switch>
           <Route exact path='/register' component={Register} />
           <Route exact path='/login' component={Login} />
         </Switch>
+        </section>
       </Fragment>
     </Router>
   </Provider>
--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,7 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuidv4();
   dispatch({
     type: SET_ALERT,
@@ -11,5 +11,8 @@ export const setAlert = (msg, alertType) => dispatch => {
       id,
     },
   });
+
+  setTimeout(() => dispatch({ type:REMOVE_ALERT, payload:id }), timeout);
+
 };
 
