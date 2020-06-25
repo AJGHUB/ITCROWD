570841a5635b6bdc82dfbdf1b78a5c5e4e9bc635
Alice Jowsey <12345alcatraz@gmail.com>
2020-06-25 20:10:26 +1200

    load user and set auth token redux tools showing

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,17 +1,40 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { 
-    REGISTER_SUCCESS, 
-    REGISTER_FAIL 
+import setAuthToken from '../utils/setAuthToken';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
 } from './types';
 
+//Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token);
+  {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('/api/auth');
+
+    dispatch({
+      type: USER_LOADED,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      AUTH_ERROR,
+    });
+  }
+};
+
 //Register User
-export const register = ({ name, email, password }) => async dispatch => {
+export const register = ({ name, email, password }) => async (dispatch) => {
   const config = {
     headers: {
       'Content-Type': 'application/json',
-    }
-  }
+    },
+  };
 
   const body = JSON.stringify({ name, email, password });
 
@@ -23,11 +46,11 @@ export const register = ({ name, email, password }) => async dispatch => {
       payload: res.data,
     });
   } catch (err) {
-      const errors = err.response.data.errors;
+    const errors = err.response.data.errors;
 
-      if(errors){
-          errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
-      }
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+    }
 
     dispatch({
       type: REGISTER_FAIL,
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,5 +2,8 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
 export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_FAIL';
+export const AUTH_ERROR = 'AUTH_ERROR';
+
 
 //centralised files for all types  - easily changed here
