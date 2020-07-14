449a81a6e07a319865efc94e114c2908d7f3de7d
Alice Jowsey <12345alcatraz@gmail.com>
2020-07-15 11:36:23 +1200

    display github repo in progress

--- client/src/components/layout/spinner.js
+++ client/src/components/layout/spinner.js
@@ -5,7 +5,7 @@ export default () => (
   <Fragment>
     <img
       src={spinner}
-      style={{ width: '200px', margin: 'auto', display: 'block' }}
+      style={{ width: '500px', margin: 'auto', display: 'block' }}
       alt='Loading...'
     />
   </Fragment>
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -361,33 +361,22 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 // @desc    Get user repos from github
 // @access  Public
 
-router.get('/github/:username', (req, res) => {
+router.get('/github/:username', async (req, res) => {
   try {
-    const options = {
-      uri: `https://api.github.com/users/${
-        req.params.username
-      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
-        'githubClientId'
-      )}&client_secret=${config.get('githubSecret')}`,
-      method: 'GET',
-      headers: {
-        'user-agent': 'node.js',
-      },
+    const uri = encodeURI (
+      `https://api.github.com/users/${
+        req.params.username}/repos?per_page=5&sort=created:asc`
+    );
+    const headers = {
+      'user-agent': 'node.js',
+      Authorization: `token ${config.get('githubToken')}`
     };
-    request(options, (error, response, body) => {
-      if (error) console.error(error);
 
-      if (response.statusCode !== 200) {
-        res.status(404).json({
-          msg: 'No Github profile found',
-        });
-      }
-
-      res.json(JSON.parse(body));
-    });
+    const gitHubResponse = await axios.get(uri, { headers });
+    return res.json(gitHubResponse.data);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    return res.status(404).json({ msg: 'No Github profile found' });
   }
 });
 
