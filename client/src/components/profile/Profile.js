1298c6955d94fccd434f7bef75d7f6fede391052
Alice Jowsey <12345alcatraz@gmail.com>
2020-07-14 13:30:22 +1200

    add experience to profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
 import { getProfileById } from '../../actions/profile';
 
 const Profile = ({
@@ -36,6 +37,21 @@ const Profile = ({
           <div className='profile-grid my-1'>
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <Fragment>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    />
+                  ))}
+                </Fragment>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
           </div>
         </Fragment>
       )}
--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -12,18 +12,16 @@ const ProfileAbout = ({
     {bio && (
       <Fragment>
         <h2 className='text-primary'>{name.trim().split(' ')[0]}s Bio</h2>
-        <p>
-         {bio}
-        </p>
+        <p>{bio}</p>
         <div className='line'></div>
       </Fragment>
     )}
     <h2 className='text-primary'>Skill Set</h2>
     <div className='skills'>
       {skills.map((skill, index) => (
-          <div key={index} className='p-1'>
-              <i className='fas fa-check' /> {skill}
-          </div>
+        <div key={index} className='p-1'>
+          <i className='fas fa-check' /> {skill}
+        </div>
       ))}
     </div>
   </div>
