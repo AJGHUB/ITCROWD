39fef96f79eac7815a226675332e5c2eec2e0c3b
Alice Jowsey <12345alcatraz@gmail.com>
2020-07-20 23:20:37 +1200

    dev conn master routes auth post posts updated

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,105 +4,96 @@ const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
 const jwt = require('jsonwebtoken');
 const config = require('config');
-const {check, validationResult} = require('express-validator'); //removed > /check
+const { check, validationResult } = require('express-validator'); //removed > /check
 
-const User = require('../../models/User')
-
+const User = require('../../models/User');
 
 // @route   GET api/auth
-// @desc    Test route 
+// @desc    Test route
 // @access  Public
 router.get('/', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id).select('-password');
-        console.log(req.user.id);
-        res.json(user);
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/auth
 // @desc    Authenticate User & get token
 // @access  Public (no token needed)
 router.post(
-    '/',
-    [
-        check('email', 'Please include a valid email')
-        .isEmail(),
-        check('password', 'Password is required')
-        .exists()
-    ],
-    async (req, res) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({
-                errors: errors.array()
-            });
-        } //async func sends an error message to the user to say above inputs are invalid
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    } //async func sends an error message to the user to say above inputs are invalid
 
-        const {
-            email,
-            password
-        } = req.body;
+    const { email, password } = req.body;
 
-        try {
-            // see if user exists if user exists send back an error
-            let user = await User.findOne({
-                email
-            });
+    try {
+      // see if user exists if user exists send back an error
+      let user = await User.findOne({
+        email,
+      });
 
-            if (!user) {
-                return res
-                    .status(400)
-                    .json({
-                        errors: [{
-                            msg: 'Invalid Credentials' //using the same terminology to back up user security 
-                        }]
-                    });
-            }
+      if (!user) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'Invalid Credentials', //using the same terminology to back up user security
+            },
+          ],
+        });
+      }
 
-            //match email and password >compare returns promise with async await
-              const isMatch = await bcrypt.compare(password, user.password)
-             
+      //match email and password >compare returns promise with async await
+      const isMatch = await bcrypt.compare(password, user.password);
 
-            if (!isMatch) {
-                return res
-                    .status(400)
-                    .json({
-                        errors: [{
-                            msg: 'Invalid Credentials' //using the same terminology to back up user security 
-                        }]
-                    });
-            }
+      if (!isMatch) {
+        return res.status(400).json({
+          errors: [
+            {
+              msg: 'Invalid Credentials', //using the same terminology to back up user security
+            },
+          ],
+        });
+      }
 
-            //return json webtoken
-            const payload = {
-                user: {
-                    id: user.id
-                }
-            }
+      //return json webtoken
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
 
-            jwt.sign(
-                payload,
-                config.get('jwtSecret'), {
-                    expiresIn: 360000
-                },
-                (err, token) => {
-                    if (err) throw err;
-                    res.json({
-                        token
-                    })
-                }
-            );
-        } catch (err) {
-            console.error(err.message);
-            res.status(500).send('Server error');
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        {
+          expiresIn: '5 days',
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({
+            token,
+          });
         }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
     }
+  }
 );
-
-
 
 module.exports = router;
