e9a0e2554ccb6c77861aa384b1f46e70dc356fda
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-03 19:50:17 +1200

    prepare for deployment

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (process.env.Node_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
