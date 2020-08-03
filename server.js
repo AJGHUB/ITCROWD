9e92845d1c1ed2cf0def4c9251f80b8a99905606
Alice Jowsey <12345alcatraz@gmail.com>
2020-08-03 20:57:05 +1200

    Node_ENV typo to NODE_ENV fix in server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 //serve static assets in production
-if (process.env.Node_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
 
